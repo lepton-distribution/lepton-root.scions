4a3da618a634066d5690cfc402e82b5b9e02a56a
lepton-phlb <lepton.phlb@gmail.com>
2017-07-17 11:46:15 +0200

    removed oldest files of lwip (lwip 1.3.2).

--- net/lwip-1.3.2/scion/sys/root/src/kernel/net/lwip/netif/ppp/chpms.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef CHPMS_H
-#define CHPMS_H
-
-#define MAX_NT_PASSWORD 256 /* Maximum number of (Unicode) chars in an NT password */
-
-void ChapMS (chap_state *, char *, int, char *, int);
-
-#endif /* CHPMS_H */
