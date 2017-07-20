5d1b3ac63c716848da208fc105c4f01a3b1346af
lepton-phlb <lepton.phlb@gmail.com>
2017-07-20 12:01:19 +0200

    added preprocessor STM32F407XX definition, for stm32 header files.

--- executive-sample/executive-base/scion/sys/user/tauon-basic/src/arch/st-stm32f407-discovery/kernel_mkconf/include/user_kernel_mkconf.h
+++ executive-sample/executive-base/scion/sys/user/tauon-basic/src/arch/st-stm32f407-discovery/kernel_mkconf/include/user_kernel_mkconf.h
@@ -14,6 +14,13 @@ Compiler Directive
    #include "kernel/core/ucore/freeRTOS_8-0-0/source/arch/cortex-m4/stm32f4/FreeRTOSConfig.h"
 #endif 
 
+//see in kernel\dev\arch\cortexm\stm32f4xx\cubemx_hal_driver\inc\legacy\stm32f4xx_hal_conf.h
+//force definition ST cpu target type
+#define STM32F407xx
+#ifndef STM32F407xx
+   #define STM32F407xx
+#endif
+
 //force definition of external clock (8MHz) on discovery board (see in stm32f4xx.h)
 #define HSE_VALUE    ((uint32_t)8000000)
 //force definition of cpu device
@@ -27,6 +34,16 @@ Compiler Directive
 //force EFFS for stm32f407 on olimex-stm32-p407 board
 #define __file_system_profile__  __file_system_profile_classic__
 #define __KERNEL_VFS_SUPPORT_EFFS   0
+#define __KERNEL_VFS_SUPPORT_FATFS  0
+
+
+//kernel printk on /dev/console
+#define __KERNEL_PRINTK
+//kernel trace_printk on /dev/trace
+#define __KERNEL_TRACE_PRINTK
+//kernel console for initd and printk dev output on /dev/console stream
+#define __KERNEL_DEV_TTY "/dev/ttys6"
+
 
 //ip stack
 //#define USE_UIP 
